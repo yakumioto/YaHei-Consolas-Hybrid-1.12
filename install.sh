74540c0c6a19c4e122f3fbb8a54454d5e188e0fa
Yaku Mioto <admin@mioto.me>
2017-02-09 11:37:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,20 +10,20 @@ git clone https://github.com/yakumioto/YaHei-Consolas-Hybrid-1.12.git /tmp
 echo 'Truetype folder Creating...'
 echo 'path:/usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid'
 
-sudo mkdir -p /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid
+mkdir -p /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid
 
 echo 'Copying font to truetype folder...'
 
-sudo cp /tmp/YaHei-Consolas-Hybrid-1.12/YaHei\ Consolas\ Hybrid\ 1.12.ttf /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid
+cp /tmp/YaHei-Consolas-Hybrid-1.12/YaHei\ Consolas\ Hybrid\ 1.12.ttf /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid
 
 echo 'Modifying font permissions...'
 
-sudo chmod 644 /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid/YaHei\ Consolas\ Hybrid\ 1.12.ttf
+chmod 644 /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid/YaHei\ Consolas\ Hybrid\ 1.12.ttf
 
 echo 'installing YaHei Consolas Hybrid font...'
 
-sudo mkfontscale
-sudo mkfontdir
-sudo fc-cache -fv
+mkfontscale
+mkfontdir
+fc-cache -fv
 
 echo 'Complete!'
